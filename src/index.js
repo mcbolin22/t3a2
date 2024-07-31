b8efc9295901cdbf714a4ca64ed9dc14d60eaf45
Colin <mcbolin@hotmail.com>
2024-07-31 16:08:59 +1000

    feat: add User Schema

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 // CONFIG (runs between)
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// Routes
+app.post("/auth/register", upload.single("picture"), register);
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001;
