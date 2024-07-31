dd5a1b404bdb4998f17b24e2cd39b82434da643b
Colin <mcbolin@hotmail.com>
2024-07-31 17:54:15 +1000

    feat: add remove friend function added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes"
 import { register } from "./controllers/auth.js";
 
 // CONFIG (runs between)
@@ -45,6 +46,7 @@ app.post("/auth/register", upload.single("picture"), register);
 //Routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001;
