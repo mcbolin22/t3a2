74680b9819a4c1e8fe3ec6e088bb50619536468d
Colin <mcbolin@hotmail.com>
2024-08-06 13:47:01 +1000

    feat: adding sports and teams routes to user

--- src/server/src/routes/users.js
+++ src/server/src/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, getUserFriends, addRemoveFriend, addRemoveSport, addRemoveTeam } from "../controllers/users.js";
+import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,10 +7,12 @@ const router = express.Router();
 // read
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
+// router.get("/:id/sports", verifyToken, getUserSports);
+// router.get("/:id/teams", verifyToken, getUserTeams);
 
 // update
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
-router.patch("/:id/:sportId", verifyToken, addRemoveSport);
-router.patch("/:id/:teamId", verifyToken, addRemoveTeam);
+// router.patch("/:id/:sportId", verifyToken, addRemoveSport);
+// router.patch("/:id/:teamId", verifyToken, addRemoveTeam);
 
 export default router;
