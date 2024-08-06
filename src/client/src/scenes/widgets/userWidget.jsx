0bfbd585b0675875432ac8caec0b3ad7d3864015
Colin <mcbolin@hotmail.com>
2024-08-06 16:18:12 +1000

    feat: User widget is complete

--- src/client/src/scenes/widgets/userWidget.jsx
+++ src/client/src/scenes/widgets/userWidget.jsx
@@ -14,6 +14,113 @@ import {
 
   const UserWidget = ({ userId, picturePath }) => {
     const [user, setUser] = useState(null);
-    const { palette }
-  }
+    const { palette } = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+
+    const getUser = async () => {
+        const response = await fetch(`http://localhost:3001/users/${userId}`,
+            {
+                method: "GET",
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+        const data = await response.json();
+        setUser(data);
+        
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    if (!user) {
+        return null;
+    }
+
+    const {
+        firstName,
+        lastName,
+        location,
+        occupation,
+        viewedProfile,
+        braggingRights,
+        impressions,
+        sports,
+        teams,
+        friends,
+    } = user;
+
+    return (
+        <WidgetWrapper>
+            {/* first row */}
+            <FlexBetween
+                gap="0.5rem"
+                pb="1.1rem"
+                onClick={() => navigate(`/profile/${userId}`)}
+            >
+                <FlexBetween gap="1rem">
+                    <UserImage image={picturePath} />
+                    <Box>
+                        <Typography 
+                            variant="h4"
+                            color={dark}
+                            fontWeight="500"
+                            sx={{
+                                "&:hover": {
+                                    color: palette.primary.light,
+                                    cursor: "pointer"
+                                }
+                            }}
+                        >
+                            {firstName} {lastName}
+                        </Typography>
+                        <Typography color={medium}>{friends.length} friends</Typography>
+                    </Box>
+                    
+                </FlexBetween>
+                <ManageAccountsOutlined />
+            </FlexBetween>
+
+            <Divider />
+
+            {/* second row */}
+            <Box p="1rem 0">
+                <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                    <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+                    <Typography color={medium}>{location}</Typography>
+                </Box>
+                <Box display="flex" alignItems="center" gap="1rem" >
+                    <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+                    <Typography color={medium}>{occupation}</Typography>
+                </Box>
+            </Box>
+
+            {/* third row */}
+            <Box p="1rem 0">
+                <FlexBetween mb="0.5rem">
+                    <Typography color={medium}>Who's viewed your profile?</Typography>
+                    <Typography color={medium} fontWeight="500">{viewedProfile}</Typography>
+                </FlexBetween>
+                <FlexBetween>
+                    <Typography color={medium}>Your "Bragging Rights":</Typography>
+                    <Typography color={medium} fontWeight="500">{braggingRights}</Typography>
+                </FlexBetween>
+                <FlexBetween>
+                <Typography color={medium}>Impressions on your posts:</Typography>
+                <Typography color={medium} fontWeight="500">{impressions}</Typography>
+                </FlexBetween>
+
+            </Box>
+
+                            
+        
+    </WidgetWrapper>
+    );
+  };
+
+  export default UserWidget;
 
