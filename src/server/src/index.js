a1408a8a2987a49dfd9015f79b0d561ccb7c2fb6
Colin <mcbolin@hotmail.com>
2024-08-07 17:16:41 +1000

    fix: register new user works

--- src/server/src/index.js
+++ src/server/src/index.js
@@ -30,12 +30,14 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "./public/assets")));
+console.log(__dirname)
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // file storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/assests");
+    console.log(file)
+    cb(null, "src/public/assets");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
